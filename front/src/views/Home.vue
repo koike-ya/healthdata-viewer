2eb0d7f79bbfd8c28cab0ecc19ca09e4772cfc96
Tomoya Koike <makeffort134@gmail.com>
2021-08-08 14:24:13 +0900

    feat: OuraのOauth認証によるaccess_token取得を実装

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <button v-on:click="connectToOura">Ouraと連携する</button>
+    <p>{{ routes }}</p>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+<script>
+import axios from 'axios'
 
-export default defineComponent({
+export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  data: function () {
+    return {
+      routes: undefined
+    }
+  },
+  mounted: async function () {
+    const code = this.extractQueryValueFromURI('code')
+    if (code) {
+      const backendUrl = 'http://localhost:3000'
+      const res = await axios.get(`${backendUrl}/auth?code=${code}`)
+      const accessToken = res.data.access_token
+      console.log(accessToken)
+    }
+  },
+  methods: {
+    connectToOura: async function () {
+      const baseUrl = 'https://cloud.ouraring.com'
+      const url = `${baseUrl}/oauth/authorize?response_type=code&client_id=${process.env.VUE_APP_OURA_CLIENT_ID}`
+      window.open(url, '_blank')
+    },
+    extractQueryValueFromURI: function (key) {
+      const uri = window.location.href.split('?')
+      if (uri.length === 2) {
+        const params = uri[1].split('&')
+        for (const p of params) {
+          const list = p.split('=')
+          if (list.length === 2 && list[0] === key) {
+            return list[1]
+          }
+        }
+      }
+    }
   }
-})
+}
 </script>
+
+<style>
+</style>
